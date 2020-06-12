9505dce7234be017222ce5669e172a064eae517c
nmarkro <nmarkro@gmail.com>
2020-06-12 12:44:57 -0500

    finish assignment

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,4 +12,8 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
+
+    shouldShowWarning() {
+        return this.type.toLowerCase() === 'space debris';
+    }
 }
