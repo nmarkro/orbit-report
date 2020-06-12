346a8f6a3b17cf46180f33155a872cef7c6d08f1
nmarkro <nmarkro@gmail.com>
2020-06-12 13:01:18 -0500

    finish bonus

---
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Satellite } from '../satellite';
+
+@Component({
+  selector: 'app-orbit-counts',
+  templateUrl: './orbit-counts.component.html',
+  styleUrls: ['./orbit-counts.component.css']
+})
+export class OrbitCountsComponent implements OnInit {
+  @Input() satellites: Satellite[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getCountOfType(type: string) {
+    return this.satellites.filter( 
+      (satellite) => satellite.type.toLowerCase() === type.toLowerCase()
+    ).length;
+  }
+}
